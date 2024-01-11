d9517b8c75499b9542268fb279eb55cc8e7c644f
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2024-01-11 14:39:49 +0100

    content, channel models added

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Content, Channel
 
-# Register your models here.
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'rating']
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'language']
+
+
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Channel, ChannelAdmin)
--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Content(models.Model):
+        
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=2500)
+    genre = models.CharField(max_length=250)
+    authors = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    rating = models.PositiveIntegerField(default=0)
+    file_url = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.name 
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Channel(models.Model):
+        
+    title = models.CharField(max_length=250)
+    language = models.CharField(max_length=250)
+    picture_url = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+    
