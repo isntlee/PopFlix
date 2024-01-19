db39213b4757d80cbbc5bee134cc47f329da0326
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2024-01-19 11:32:03 +0100

    find_parents.py command added, for development

--- contents/management/commands/find_parents.py
+++ contents/management/commands/find_parents.py
@@ -1,16 +1,13 @@
 from django.core.management.base import BaseCommand
 from contents.models import Channel, Content
-import csv
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        # This will have to be changed, slug='empire'
-
-        content_obj = Content.objects.get(slug='empire')    
+        content_obj = Content.objects.filter(slug='empire').first()    
         channel = Content.get_channel(content_obj)
         parents = Channel.get_all_parents(channel)
 
-        return parents
+        print('\n\n## parents_list:', parents, '\n\n')
--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+#You'll have to remove this later
+CACHE_MIDDLEWARE_SECONDS = 0
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
